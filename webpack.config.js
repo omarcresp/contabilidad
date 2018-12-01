bf4dfc0f5fbf3818ae259509660a94ebc58244e7
omar crespo <crespomerchano@gmail.com>
2018-12-01 07:17:35 -0400

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,18 @@
 module.exports = {
-    mode: 'production',
-    entry: './src/app.js'
+  mode: 'production',
+  entry: './src/app.js',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 }
